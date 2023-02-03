75e99f70efcc4337065f1ae3f72494f15409473b
Andin Gege <marpaungandin@gmail.com>
2023-02-03 19:28:33 +0700

    Register cermati

---
+++ Test Suites/Register_cermati.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Register_cermati</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>false</rerunImmediately>
+   <testSuiteGuid>031a61bd-04da-4f7d-a367-9e18ac0f89ec</testSuiteGuid>
+   <testCaseLink>
+      <guid>f361d79f-b606-4339-8623-3af03fc29781</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test_Cermati/Daftar_Cermati</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
